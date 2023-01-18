a204d039e2eab981193864fc4172bce1c6f296ae
Ramiro Gordon <ramirogordon2@gmail.com>
2023-01-18 15:46:35 -0300

    error-handler middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { json } from "body-parser";
 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
+import {
+  currentUserRouter,
+  signinRouter,
+  signoutRouter,
+  signupRouter,
+} from "./routes";
 import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
