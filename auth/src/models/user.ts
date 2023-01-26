73382623b48c14cf1f8bd265c16704d96e333e7c
Ramiro Gordon <ramirogordon2@gmail.com>
2023-01-26 10:22:21 -0300

    making last changes to User model with TS

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -5,8 +5,13 @@ interface UserAttrs {
   password: string;
 }
 
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
 }
 
 const userSchema = new mongoose.Schema({
@@ -24,6 +29,6 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>("User", userSchema);
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 
 export { User };
