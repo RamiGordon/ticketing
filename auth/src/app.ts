12adae0ba34a03a4de130964de085eff8942ff22
Ramiro Gordon <ramirogordon2@gmail.com>
2023-04-28 13:04:10 -0300

    decouple app from start up settings

---
+++ auth/src/app.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import {
+  currentUserRouter,
+  signinRouter,
+  signoutRouter,
+  signupRouter,
+} from "./routes";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFountError } from "./errors/not-found-error";
+
+const app = express();
+// we have ingress doing a proxy for us, this to tell express to trust ingress
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async () => {
+  throw new NotFountError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,39 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import {
-  currentUserRouter,
-  signinRouter,
-  signoutRouter,
-  signupRouter,
-} from "./routes";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFountError } from "./errors/not-found-error";
-
-const app = express();
-// we have ingress doing a proxy for us, this to tell express to trust ingress
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async () => {
-  throw new NotFountError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   const authPodName = "auth-mongo-srv";
