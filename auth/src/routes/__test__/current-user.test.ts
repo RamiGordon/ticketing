db09298580eed996f886420b4e0f7a3c66bccdc4
Ramiro Gordon <ramirogordon2@gmail.com>
2023-04-29 22:57:05 -0300

    global signin added to have consistency between test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,15 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("clears the cookie agter signing out", async () => {
-  const authResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
-
-  const cookie = authResponse.get("Set-Cookie");
+  const cookie = await global.signin();
 
   const response = await request(app)
     .get("/api/users/currentuser")
