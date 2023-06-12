d8f5c54fc46e002539ed31a81dad15ef4117ab42
Ramiro Gordon <ramirogordon2@gmail.com>
2023-06-12 00:34:18 -0300

    signin page added

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,17 @@
-const LandingPage = () => {
-  return <h1>Hello There!!</h1>;
+import buildClient from "../api/buildClient";
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in {currentUser.email}</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
+};
+
+LandingPage.getInitialProps = async (ctx) => {
+  const { data } = await buildClient(ctx).get("/api/users/currentuser");
+
+  return data;
 };
 
 export default LandingPage;
