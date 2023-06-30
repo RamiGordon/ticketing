c2971b69fd6941259f84ebd404b825188b204f7f
Ramiro Gordon <ramirogordon2@gmail.com>
2023-06-30 14:09:14 -0300

    tickets module added correctly

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,8 +1,12 @@
-import { errorHandler, NotFountError } from "@alakator/common";
+import { errorHandler, NotFountError, currentUser } from "@alakator/common";
 import cookieSession from "cookie-session";
 import { json } from "body-parser";
 import express from "express";
 import "express-async-errors";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 // we have ingress doing a proxy for us, this to tell express to trust ingress
@@ -14,6 +18,12 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async () => {
   throw new NotFountError();
